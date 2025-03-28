cd84e3f8e36c85d08b367bad64a96947927d54e6
Carlos Ceballo <carlos96cacv@gmail.com>
2025-03-28 18:30:20 -0400

    CC bug fixed db

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const dbConnect = mysql.createConnection({
@@ -6,6 +6,11 @@ const dbConnect = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  idleTimeout: 60000,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000,
 });
 
 dbConnect.connect((err) => {
